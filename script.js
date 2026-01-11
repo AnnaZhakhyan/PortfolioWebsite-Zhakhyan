5cba53ce3b241d7744368c9653ca59f60b11c088
AnnaZhakhyan <150126484+AnnaZhakhyan@users.noreply.github.com>
2026-01-11 19:16:01 +0100

    starting

---
+++ script.js
@@ -0,0 +1,13 @@
+const animatedElements = document.querySelectorAll('.animate');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+animatedElements.forEach(el => observer.observe(el));
